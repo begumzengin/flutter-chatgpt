bb39a9f36ce7c2e6d338024c212b9eda609716c5
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-02-22 22:34:02 +0300

    Create chat_widget.dart

---
+++ flutter_chatgpt/lib/widgets/chat_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_chatgpt/constants/constants.dart';
+import 'package:flutter_chatgpt/services/assets_manager.dart';
+
+class ChatWidget extends StatelessWidget {
+  const ChatWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Material(
+          color: cardColor,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [Image.asset(AssetsManager.userImage)],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
